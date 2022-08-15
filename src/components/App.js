21a8a6e0c7f6dbbea23f5989b7adccfe17c87103
unknown <IDISuperAdmin@IDIDomain2020.com>
2022-08-15 15:50:59 +0200

    First Commit

---
+++ src/components/App.js
@@ -0,0 +1,65 @@
+//Imports
+import { useState, useEffect } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import ListContacts from "./ListContacts";
+import CreateContact from "./CreateContact.js";
+import * as ContactsAPI from "../utils/ContactsAPI";
+import "../css/App.css";
+
+
+//Coding 
+const App = () => 
+{
+  let navigate = useNavigate();
+
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const getContacts = async () => {
+      const res = await ContactsAPI.getAll();
+      setContacts(res);
+    };
+
+    getContacts();
+  }, []);
+
+  const removeContact = (contact) => {
+    ContactsAPI.remove(contact);
+
+    setContacts(contacts.filter((c) => c.id !== contact.id));
+  };
+
+  const createContact = (contact) => {
+    const create = async () => {
+      const res = await ContactsAPI.create(contact);
+      setContacts(contacts.concat(res));
+    };
+
+    create();
+    navigate("/");
+  };
+
+  return (
+    <Routes>
+      <Route
+        exact
+        path="/"
+        element={
+          <ListContacts contacts={contacts} onDeleteContact={removeContact} />
+        }
+      />
+      <Route
+        path="/create"
+        element={
+          <CreateContact
+            onCreateContact={(contact) => {
+              createContact(contact);
+            }}
+          />
+        }
+      />
+    </Routes>
+  );
+};//end App
+
+export default App;
